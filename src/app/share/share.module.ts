0c4cf8f4859c0b7e30a0a36759721e55954188b8
Xiang Song <himmelsong@gmail.com>
2021-01-30 11:17:32 -0700

    add header/footer, home mudule/component/route

--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzCarouselModule } from 'ng-zorro-antd/carousel';
 
 
 
@@ -15,7 +20,12 @@ import { NzButtonModule } from 'ng-zorro-antd/button';
   exports: [
     CommonModule,
     FormsModule,
-    NzButtonModule ,
+    NzButtonModule,
+    NzLayoutModule,
+    NzMenuModule,
+    NzIconModule,
+    NzInputModule,
+    NzCarouselModule,
   ]
 })
 export class ShareModule { }
