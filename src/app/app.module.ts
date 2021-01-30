cf623c1b9aa419e353aa5a95a21e210492d455bb
Xiang Song <himmelsong@gmail.com>
2021-01-30 10:53:14 -0700

    modulize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,16 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NZ_I18N } from 'ng-zorro-antd/i18n';
-import { en_US } from 'ng-zorro-antd/i18n';
-import { registerLocaleData } from '@angular/common';
-import en from '@angular/common/locales/en';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NzButtonModule } from 'ng-zorro-antd/button';
+import { CoreModule } from './core/core.module';
 
-registerLocaleData(en);
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
-    NzButtonModule ,
-    BrowserAnimationsModule
+    CoreModule
   ],
-  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,39 @@
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from '../app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServicesModule } from '../services/services.module';
+import { PagesModule } from '../pages/pages.module';
+import { ShareModule } from '../share/share.module';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { NZ_I18N, en_US } from 'ng-zorro-antd/i18n';
+
+
+registerLocaleData(en);
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ServicesModule,
+    PagesModule,
+    ShareModule,
+    AppRoutingModule,
+  ],
+  exports: [
+    ShareModule,
+    AppRoutingModule,
+  ],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
+})
+export class CoreModule { 
+  constructor(@SkipSelf() @Optional() parentModule: CoreModule) {
+    if (parentModule) {
+      throw new Error('CoreModule can only be imported by appModule');
+    }
+  }
+}
